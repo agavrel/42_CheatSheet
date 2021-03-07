1f78f1fb5e05c5c092fdfb978852ff1fc5abd67c
agavrel <gavrel.antonin@gmail.com>
2021-03-07 14:57:14 -0500

    [ADD] Print binary recursively

--- functions/anagram.c
+++ functions/anagram.c
@@ -3,13 +3,12 @@
 int t[256],i;
 int main(int c)
 {
-volatile char *const chptr;
-    for(;c+3;)
-    {
-    printf("%d\n" , c);
+    volatile char *const chptr;
+    for(;c+3;) {
+        printf("%d\n" , c);
         (i=getchar())>10?t[i]+=c:(c-=2);
         printf("%d\n" , c);
-}
+    }
     for(i=257;--i&&!t[i-1];);
-    puts(i?"false":"true");
+        puts(i?"false":"true");
 }
---
+++ functions/print_binary_recurs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int base(int n, int mask)
+{
+    if(!mask) {
+        printf("\n"); // we reach the end, print a line return
+        return 0;
+    }
+    printf("%d",  !!(n & mask)); // if mask and n match, print '1', else print '0'. !! convert any value into 1, and 0 remains 0.
+    return base(n, mask >> 1); // divide mask by 2, check the next bit on the right
+}
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    base(n, 1 << (31 - __builtin_clz(n))); // call the function with a mask initialized at the same level than the most important bit of n.
+    return 0;
+}
